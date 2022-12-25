ec8656af38798eb7bd8fbabc40d3f3d5b6580855
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-25 12:24:30 +0300

    Task_50

---
+++ Task_50/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > matrix.GetLength(0) || m > matrix.GetLength(1))
+{
+    Console.WriteLine("Такой позиции в массиве нет!");
+}
+else
+{
+    Console.WriteLine($"Значение элемента = {matrix[n - 1, m - 1]}");
+}
