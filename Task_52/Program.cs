1d8058762de9c3b64819f02aa4383cc9872e4205
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-25 12:24:42 +0300

    Task_52

---
+++ Task_52/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Release(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.Write($"{ sum / matrix.GetLength(0)} ");
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Массив: ");
+InputMatrix(matrix);
+Console.WriteLine("Среднее арифметическое: ");
+Release(matrix);
